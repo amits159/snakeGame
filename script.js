632aebde9db04686e55b562fdc6641460db7f00d
amits159 <93002706+amits159@users.noreply.github.com>
2021-12-03 23:49:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,34 @@
-// GAME_PIXEL_COUNT is the pixels on horizontal or vertical axis of the game board (SQUARE).
 const GAME_PIXEL_COUNT = 40;
 const SQUARE_OF_GAME_PIXEL_COUNT = Math.pow(GAME_PIXEL_COUNT, 2);
 
 let totalFoodAte = 0;
-let totalDistanceTravelled = 0;
 
-/// THE GAME BOARD:
-const gameContainer = document.getElementById("gameContainer");
+const container = document.getElementById("container");
 
 const createGameBoardPixels = () => {
-  // Populate the [#gameContainer] div with small div's representing game pixels
   for (let i = 1; i <= SQUARE_OF_GAME_PIXEL_COUNT; ++i) {
-    gameContainer.innerHTML = `${gameContainer.innerHTML} <div class="gameBoardPixel" id="pixel${i}"></div>`;
+    container.innerHTML = `${container.innerHTML} <div class="gameBoardPixel" id="pixel${i}"></div>`;
   }
 };
 
-// This variable always holds the updated array of game pixels created by createGameBoardPixels() :
 const gameBoardPixels = document.getElementsByClassName("gameBoardPixel");
 
-/// THE FOOD:
 let currentFoodPostion = 0;
 const createFood = () => {
-  // Remove previous food;
   gameBoardPixels[currentFoodPostion].classList.remove("food");
-
-  // Create new food
   currentFoodPostion = Math.random();
-  currentFoodPostion = Math.floor(
-    currentFoodPostion * SQUARE_OF_GAME_PIXEL_COUNT
-  );
+  currentFoodPostion = Math.floor(currentFoodPostion * SQUARE_OF_GAME_PIXEL_COUNT);
   gameBoardPixels[currentFoodPostion].classList.add("food");
 };
 
-/// THE SNAKE:
-
-// Direction codes (Keyboard key codes for arrow keys):
 const LEFT_DIR = 37;
 const UP_DIR = 38;
 const RIGHT_DIR = 39;
 const DOWN_DIR = 40;
 
-// Set snake direction initially to right
 let snakeCurrentDirection = RIGHT_DIR;
 
 const changeDirection = newDirectionCode => {
-  // Change the direction of the snake
   if (newDirectionCode == snakeCurrentDirection) return;
 
   if (newDirectionCode == LEFT_DIR && snakeCurrentDirection != RIGHT_DIR) {
@@ -61,13 +45,10 @@ const changeDirection = newDirectionCode => {
   }
 };
 
-// Let the starting position of the snake be at the middle of game board
 let currentSnakeHeadPosition = SQUARE_OF_GAME_PIXEL_COUNT / 2;
 
-// Initial snake length
-let snakeLength = 1000;
+let snakeLength = 500;
 
-// Move snake continously by calling this function repeatedly :
 const moveSnake = () => {
   switch (snakeCurrentDirection) {
     case LEFT_DIR:
@@ -111,13 +92,12 @@ const moveSnake = () => {
 
   let nextSnakeHeadPixel = gameBoardPixels[currentSnakeHeadPosition];
 
-  // Kill snake if it bites itself:
   if (nextSnakeHeadPixel.classList.contains("snakeBodyPixel")) {
-    // Stop moving the snake
     clearInterval(moveSnakeInterval);
     if (
       !alert(
-        `You have ate ${totalFoodAte} food by travelling ${totalDistanceTravelled} blocks.`
+        ` GAME OVER
+Your Score is ${totalFoodAte}.`
       )
     )
       window.location.reload();
@@ -129,44 +109,17 @@ const moveSnake = () => {
     nextSnakeHeadPixel.classList.remove("snakeBodyPixel");
   }, snakeLength);
 
-  // Update total distance travelled
-  totalDistanceTravelled++;
-  // Update in UI:
-  document.getElementById("blocksTravelled").innerHTML = totalDistanceTravelled;
-
   if (currentSnakeHeadPosition == currentFoodPostion) {
-    // Update total food ate
     totalFoodAte++;
-    // Update in UI:
     document.getElementById("pointsEarned").innerHTML = totalFoodAte;
-
-    // Increase Snake length:
     snakeLength = snakeLength + 100;
     createFood();
   }
 };
 
-/// CALL THE FOLLOWING FUNCTIONS TO RUN THE GAME:
-
-// Create game board pixels:
 createGameBoardPixels();
-
-// Create initial food:
 createFood();
-
-// Move snake:
 var moveSnakeInterval = setInterval(moveSnake, 80);
 
-// Call change direction function on keyboard key-down event:
 addEventListener("keydown", e => changeDirection(e.keyCode));
 
-// ON SCREEN CONTROLLERS:
-const leftButton = document.getElementById("leftButton");
-const rightButton = document.getElementById("rightButton");
-const upButton = document.getElementById("upButton");
-const downButton = document.getElementById("downButton");
-
-leftButton.onclick = () => changeDirection(LEFT_DIR);
-rightButton.onclick = () => changeDirection(RIGHT_DIR);
-upButton.onclick = () => changeDirection(UP_DIR);
-downButton.onclick = () => changeDirection(DOWN_DIR);
